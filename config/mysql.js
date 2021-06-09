03ff244462036998cb0731807b828a48245a9f10
firmansyah <emank.tunk@gmail.com>
2021-06-10 00:26:03 +0700

    update

--- config/mysql.js
+++ config/mysql.js
@@ -1,15 +1,15 @@
 var mysql = require('mysql');
 
 var conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "sekolah_api"
-  // host: "https://www.phpmyadmin.co/index.php",
-  // server: "sql6.freemysqlhosting.net",
-  // user: "sql6417348",
-  // password: "cckq72NaeZ",
-  // database: "sql6417348"
+  // host: "localhost",
+  // user: "root",
+  // password: "password",
+  // database: "sekolah_api"
+  
+  server: "sql6.freemysqlhosting.net",
+  user: "sql6417348",
+  password: "cckq72NaeZ",
+  database: "sql6417348"
 });
 
 conn.connect(function (err){
